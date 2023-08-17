18f859a9800493250f4d75725efe440bc82d5349
Pedro-Caixa <pedro.schutz192@gmail.com>
2023-08-17 17:34:19 -0300

    a

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const port = process.env.PORT || 12121;
-const server = app_1.app.listen(port, () => console.log(`Listening on port ${port}\nhttp://localhost:${port}`));
-process.on('SIGINT', () => {
-    server.close();
-    console.log(`Application was interrupted`);
-});
