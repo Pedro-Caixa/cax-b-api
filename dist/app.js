de1c865581633b4d8c461ce53634dac49818d07e
Pedro-Caixa <pedro.schutz192@gmail.com>
2023-08-19 15:38:26 -0300

    General chanegs on how to run the app

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const swagger_json_1 = __importDefault(require("./swagger.json"));
@@ -14,7 +13,6 @@ exports.app = (0, express_1.default)();
 // Middleware config
 exports.app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 exports.app.use(express_1.default.json());
-exports.app.use((0, cors_1.default)());
 exports.app.use((0, morgan_1.default)('dev'));
 // ENDPOINT INTEGRATION
 exports.app.use('/api/v1', index_1.router);
